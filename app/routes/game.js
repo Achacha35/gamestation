09fead02a7a58363d0dac590523c2a9c35289b63
Achacha <achachapeter@gmail.com>
2017-07-21 11:35:18 +0300

    Linking to routes

--- app/routes/game.js
+++ app/routes/game.js
@@ -2,6 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('game', params.game_id)
+    return this.store.findRecord('game', params.game_id);
   },
+  actions: {
+    update(game, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          game.set(key, params[key]);
+        }
+      });
+      game.save();
+      this.transitionTo('index');
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -51,4 +51,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('game');
   },
+
+  actions: {
+    saveGame3(params) {
+      var newGame = this.store.createRecord('game', params);
+      newGame.save();
+      this.transitionTo('index');
+    }
+  }
 });
