60676a4f4f136b8abe92930fcffd1c8641bde4f3
Achacha <achachapeter@gmail.com>
2017-07-21 10:54:18 +0300

    Adding a dynamic segment

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('game', {path: '/game/:game_id'});
 });
 
 export default Router;
