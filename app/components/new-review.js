c96eb6e85153445dffdeb333b12411189ef4b543
Peter Achacha <achachapeter@gmail.com>
2017-07-24 16:14:36 +0300

    Update new-review.js

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -6,7 +6,7 @@ export default Ember.Component.extend({
     saveReview() {
      var params = {
        author: this.get('author'),
-       rating: this.get('rating'),
+       rating: parseInt(this.get('rating')),
        content: this.get('content'),
        game: this.get('game')
      };
